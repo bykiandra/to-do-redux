c62bb3e4ff55dd1b7edfc8f8f1f7260920970a3a
Kiandra Ginevra <kiandrajayne@gmail.com>
2021-12-07 12:17:28 +1100

    Add localstorage functions.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { createStore, combineReducers } from 'redux'
 import todosReducer from '../features/todos/todosSlice'
 import categoriesReducer from '../features/categories/categoriesSlice'
 
-export const store = configureStore({
-  reducer: {
-    todos: todosReducer,
-    categories: categoriesReducer,
-  },
+const rootReducers = combineReducers({
+  todos: todosReducer,
+  categories: categoriesReducer
 })
 
+const saveToLocalStorage = (state: RootState) => {
+  try {
+    const serialisedState = JSON.stringify(state)
+    localStorage.setItem('cf-todoReduxState', serialisedState)
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+const loadFromLocalStorage = () => {
+  try {
+    const serialisedState = localStorage.getItem('cf-todoReduxState')
+    if (serialisedState === null) return undefined
+    return JSON.parse(serialisedState)
+  } catch(err) {
+    console.log(err)
+    return undefined
+  }
+}
+
+const store = createStore(rootReducers, loadFromLocalStorage())
+store.subscribe(() => saveToLocalStorage(store.getState()))
+
 export type RootState = ReturnType<typeof store.getState>
+
+export default store
